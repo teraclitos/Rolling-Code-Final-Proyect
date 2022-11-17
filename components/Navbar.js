45a6815873a0913703cac826248a7f68a0e91902
andreahongn <andreahongn@gmail.com>
2022-11-17 13:08:17 -0300

    Subo Navbar

---
+++ components/Navbar.js
@@ -0,0 +1,77 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import BSNavbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faStar,
+  faHeart,
+  faEnvelope,
+  faRightToBracket,
+  faMagnifyingGlass,
+} from "@fortawesome/free-solid-svg-icons";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
+const Navbar = () => {
+  return (
+    <BSNavbar
+      collapseOnSelect
+      expand="lg"
+      bg="dark"
+      variant="dark"
+      sticky="top"
+    >
+      <Container fluid>
+        <BSNavbar.Brand href="#home">
+          {" "}
+          <img
+            alt=""
+            src="\logo-removebg-preview.png"
+            width="75"
+            height="75"
+            className="d-inline-block align-top"
+          />{" "}
+        </BSNavbar.Brand>
+        <BSNavbar.Toggle aria-controls="responsive-navbar-nav" />
+        <BSNavbar.Collapse
+          id="responsive-navbar-nav"
+          className="justify-content-end"
+        >
+          <Nav>
+            <Nav.Link href="#home">
+              Favoritos
+              <FontAwesomeIcon icon={faHeart} className="mx-2" />
+            </Nav.Link>
+            <Nav.Link href="">
+              Destacados
+              <FontAwesomeIcon icon={faStar} className="mx-2" />
+            </Nav.Link>
+            <Nav.Link href="">
+              Contacto
+              <FontAwesomeIcon icon={faEnvelope} className="mx-2" />
+            </Nav.Link>
+            <Nav.Link href="">
+              Iniciar sesión
+              <FontAwesomeIcon icon={faRightToBracket} className="mx-2" />
+            </Nav.Link>
+          </Nav>
+          <Form className="d-flex d-xl-none d-lg-none">
+            <Form.Control
+              type="search"
+              placeholder="Buscar"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-secondary">
+              {" "}
+              <FontAwesomeIcon icon={faMagnifyingGlass} className="mx-2" />
+            </Button>
+          </Form>
+        </BSNavbar.Collapse>
+      </Container>
+    </BSNavbar>
+  );
+};
+
+export default Navbar;
