239ad8e307555b208a8ee5ada4ba887f5025b9ac
Marina Bianconi <marinabi2306@gmail.com>
2022-11-17 19:26:25 -0300

    agrego componentes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Navbar from "../src/components/Navbar";
-
+import Footer from '../src/components/Footer';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFutbol } from "@fortawesome/free-solid-svg-icons";
 
@@ -9,6 +9,8 @@ function App() {
       <Navbar />
       <h1 className="mb-5 mt-5 fs-1">Blog grupo 2</h1>
       <FontAwesomeIcon icon={faFutbol} />
+      <Footer />
+
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import '../styles/footer.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHashtag, faLaptop, faTornado, faTwitter  } from '@fortawesome/free-solid-svg-icons'
+
+const Footer = () => {
+  return (
+    <div>
+  
+    <footer  className='mt-auto background-footer'>
+    <div className='d-flex flex-column align-items-center'>
+      <div className="row footer-contenido m-0 justify-content-center p-0 pt-4 text-white w-100">
+        <div className="col-12 col-sm-6 col-md-3 p-2 mt-4 d-flex justify-content-center w-auto align-items-center m-2">
+        {/* <Link to='/' className='m-0 p-0 d-flex flex-column align-items-center text-decoration-none'/>  */}
+        <img src='./logoRollingneta.png' width='100'  alt='logo'/> 
+        </div>  
+      <div className="col-12 col-sm-6 col-md-4 p-0 d-flex flex-column align-items-center">
+      <div className=" w-50">
+      <div className="row m-2 justify-content-center ">
+      <h6 className="my-3 fw-bold text-center mt-5">REDES SOCIALES</h6>
+        <div className="col-3  btn-footer" >
+        {/* agregar link a icono */}
+        <FontAwesomeIcon icon={faHashtag} /> 
+       
+        </div>
+        <div className="col-3 btn-footer">
+        {/* agregar link a icono */}
+        <FontAwesomeIcon  icon={faLaptop} />
+        </div>
+        <div className="col-3 btn-footer">
+        {/* agregar link a icono */}
+        <FontAwesomeIcon icon={faLaptop} />
+        </div>
+        <div class="col-3 btn-footer">
+        {/* agregar link a icono */}
+        <FontAwesomeIcon icon={faLaptop} />
+        </div>
+      </div>
+      </div>
+      </div>
+      <div className="col-12 col-sm-6 col-md-4 p-0 d-flex flex-column align-items-center">
+      <div class="row m-2 justify-content-center">
+      <h6 className="my-3 fw-bold text-center mt-5">CONTACTANOS</h6>
+      <h8 className="text-center">Av. Siempre Vivas</h8>
+      <h8 className='text-center'>A cerca de Nosotros</h8>
+      
+    </div>
+     </div>
+        <div className="col-xs-12 p-0 mt-4 copyright">
+                <p className="m-0 my-2 text white text-center">COPYRIGHT© - GRUPO 2 - PROYECTO Rollingneta - 2022</p>
+      </div>
+      </div>
+      
+    </div>  
+    </footer>
+    
+    
+    </div>
+  );
+};
+
+export default Footer;
+
