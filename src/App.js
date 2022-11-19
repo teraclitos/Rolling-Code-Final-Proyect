a428ba7d82219cec852542736da3a1702959815e
andreahongn <andreahongn@gmail.com>
2022-11-19 19:25:09 -0300

    fake api

--- src/App.js
+++ src/App.js
@@ -4,12 +4,39 @@ import Articlepublicitygrid from "../src/components/Articlepublicitygrid";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Publicidad from "./components/Publicidad";
 import Contact from "./components/Contact";
-
+import { useEffect, useState } from "react";
 function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((json) => setData(json));
+  }, []);
+
+  //carrito de mg
+  const [cart, setCart] = useState([]);
+  useEffect(() => {
+    console.log(cart);
+  }, [cart]);
+  //CART
+  const add = (p) => {
+    setCart([...cart, p]);
+    console.log("hola");
+  };
+
+  const del = (p) => {
+    console.log(p);
+  };
+
+  const clear = () => {};
+  //para navbar
+
+  const total = () => {};
+
   return (
     <div>
-      <Navbar />
-      <Articlepublicitygrid />
+      <Navbar cart={cart} />
+      <Articlepublicitygrid data={data} add={add} />
       <Contact />
       <Footer />
     </div>
--- src/components/Articlepublicitygrid.js
+++ src/components/Articlepublicitygrid.js
@@ -5,33 +5,31 @@ import { Button, Card } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
 
-const Articlepublicitygrid = () => {
+const Articlepublicitygrid = ({ data, add }) => {
   return (
     <div className="container grid-articles-publicity mt-5  ">
       <div className="grid-articles">
         <div className="mb-3 ">
-          <Card className="border-0">
-            <Card.Img
-              variant="top"
-              src="https://media.ambito.com/p/9e1a0deee64c500875512338d2a1490a/adjuntos/239/imagenes/040/223/0040223837/entradas-mundial-qatarjpg.jpg"
-            />
-            <Card.Body className="p-0 ps-1 ">
-              <h3 className="category-title fs-6 mt-1 mb-0">Fútbol</h3>
-              <Card.Title className="mt-1 mb-0">Mundial</Card.Title>
-              <Card.Text className="mt-1 mb-0">
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </Card.Text>
-            </Card.Body>
-            <Card.Footer className="d-flex justify-content-between p-0 px-1 border-0 mt-2 bg-white ">
-              <Button className="py-1 px-2 btn-color ">Leer más</Button>
-              <FontAwesomeIcon
-                className="align-self-center fs-5 text-danger"
-                icon={faHeart}
-              />
-            </Card.Footer>
-          </Card>
+          {data.map((d, i) => (
+            <Card className="border-0">
+              <Card.Img variant="top" src={d.image} />
+              <Card.Body className="p-0 ps-1 ">
+                <h3 className="category-title fs-6 mt-1 mb-0">Fútbol</h3>
+                <Card.Title className="mt-1 mb-0">{d.title}</Card.Title>
+                <Button className="py-1 px-2 btn-color " onClick={() => add(d)}>
+                  <FontAwesomeIcon
+                    className="align-self-center fs-5 text-danger"
+                    icon={faHeart}
+                  />
+                </Button>
+
+                <Card.Text className="mt-1 mb-0">{d.description}</Card.Text>
+              </Card.Body>
+              <Card.Footer className="d-flex justify-content-between p-0 px-1 border-0 mt-2 bg-white ">
+                <Button className="py-1 px-2 btn-color ">Leer más</Button>
+              </Card.Footer>
+            </Card>
+          ))}
         </div>
         <div className="mb-3 ">
           <Card className="border-0">
