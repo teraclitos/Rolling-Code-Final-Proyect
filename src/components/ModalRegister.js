61ebc293a9baf6e94334d0bb5d19d6cefd8e81f8
teraclitos <tefans12@gmail.com>
2022-11-23 13:28:01 -0300

    agrego modal registro

---
+++ src/components/ModalRegister.js
@@ -0,0 +1,41 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Modal from "react-bootstrap/Modal";
+const ModalRegister = ({ showRegister, setShowRegister }) => {
+  const handleCloseRegister = () => setShowRegister(false);
+  const handleShowRegister = () => setShowRegister(true);
+  return (
+    <Modal show={showRegister} onHide={handleCloseRegister}>
+      <Modal.Header closeButton>
+        <Modal.Title>Modal heading</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="name@example.com"
+              autoFocus
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+            <Form.Label>Example textarea</Form.Label>
+            <Form.Control as="textarea" rows={3} />
+          </Form.Group>
+        </Form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={handleCloseRegister}>
+          Close
+        </Button>
+        <Button variant="primary" onClick={handleCloseRegister}>
+          Save Changes
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+export default ModalRegister;
