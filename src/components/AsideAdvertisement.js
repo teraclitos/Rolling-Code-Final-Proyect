61467013a0b3dcbd8a7f17e349020b911beeb6e1
teraclitos <tefans12@gmail.com>
2022-11-30 13:36:33 -0300

    mejoro publicicad de la derecha

---
+++ src/components/AsideAdvertisement.js
@@ -0,0 +1,71 @@
+import React from "react";
+import Carousel from "react-bootstrap/Carousel";
+
+const AsideAdvertisement = () => {
+  return (
+    <>
+      <Carousel
+        className="carousel-advertisement sticky-top"
+        controls={false}
+        indicators={false}
+        slide={false}
+        touch={false}
+        pause={false}
+      >
+        <Carousel.Item interval={1500}>
+          <img
+            src="\publicidad-first.png"
+            className="d-block w-100"
+            alt="First slide"
+          />
+          <Carousel.Caption></Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={1500}>
+          <img
+            className="d-block w-100"
+            src="\publicidad-2.png"
+            alt="Second slide"
+          />
+          <Carousel.Caption></Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={1500}>
+          <img
+            className="d-block w-100"
+            src="\publicidad-third.png"
+            alt="Third slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item interval={1500}>
+          <img
+            className="d-block w-100"
+            src="\publicad-4-2-3.png"
+            alt="fourth slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item interval={1500}>
+          <img
+            className="d-block w-100"
+            src="\publicidad-4-medio.png"
+            alt="fifth slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item interval={1500}>
+          <img
+            className="d-block w-100"
+            src="\publicidad-4-3.png"
+            alt="sixth slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item interval={1500}>
+          <img
+            className="d-block w-100"
+            src="\publicidad-5.png"
+            alt="seventh slide"
+          />
+        </Carousel.Item>
+      </Carousel>
+    </>
+  );
+};
+
+export default AsideAdvertisement;
--- src/components/Categorias.js
+++ src/components/Categorias.js
@@ -5,98 +5,42 @@ import Container from "react-bootstrap/esm/Container";
 import Button from "react-bootstrap/Button";
 import Collapse from "react-bootstrap/Collapse";
 import ListGroup from "react-bootstrap/ListGroup";
-import Carousel from "react-bootstrap/Carousel";
+
 import "../styles/allcss.css";
 
 const Categorias = (data) => {
   const [open, setOpen] = useState(false);
 
   return (
-    <Container>
-      <Col>
-        <Row className="d-flex justify-content-center">
-          <Button
-            className="mb-3 mt-5  btn-categories"
-            variant="outline-secondary "
-            onClick={() => setOpen(!open)}
-            aria-controls="example-collapse-text"
-            aria-expanded={open}
-          >
-            Categorías
-          </Button>
-          <Collapse className="collapse-estilos" in={open}>
-            <div id="example-collapse-text  ">
-              <ListGroup className=" mb-5 list-categories">
-                <ListGroup.Item className="item-list-categories link-category ">
-                  Fútbol
-                </ListGroup.Item>
-                <ListGroup.Item className="item-list-categories link-category ">
-                  Tenis
-                </ListGroup.Item>
-                <ListGroup.Item className="item-list-categories link-category">
-                  Rugby
-                </ListGroup.Item>
-                <ListGroup.Item className="item-list-categories link-category">
-                  Atletismo
-                </ListGroup.Item>
-                <ListGroup.Item className="item-list-categories link-category">
-                  Boxeo
-                </ListGroup.Item>
-                <ListGroup.Item className="item-list-categories link-category">
-                  Voley
-                </ListGroup.Item>
-                <ListGroup.Item className="item-list-categories link-category">
-                  Básquet
-                </ListGroup.Item>
-                <ListGroup.Item className="item-list-categories link-category">
-                  Handball
-                </ListGroup.Item>
-                <ListGroup.Item className="item-list-categories link-category">
-                  Polo
-                </ListGroup.Item>
-                <ListGroup.Item className="item-list-categories link-category">
-                  Otros
-                </ListGroup.Item>
-              </ListGroup>
-            </div>
-          </Collapse>
-        </Row>
-        <Row className="d-none d-lg-block ">
-          <Carousel
-            className="carrousel-advertisement"
-            controls={false}
-            indicators={false}
-            slide={false}
-            touch={false}
-            pause={false}
-          >
-            <Carousel.Item interval={1500}>
-              <img
-                className="d-block w-100"
-                src="https://www.latinspots.com/files/notas/Gpowerade_beijing_08_web.jpg"
-                alt="First slide"
-              />{" "}
-              <Carousel.Caption></Carousel.Caption>{" "}
-            </Carousel.Item>{" "}
-            <Carousel.Item interval={1500}>
-              <img
-                className="d-block w-100"
-                src="https://media.informabtl.com/wp-content/uploads/2011/03/Puma-Social-768x512.jpg"
-                alt="Second slide"
-              />{" "}
-              <Carousel.Caption></Carousel.Caption>{" "}
-            </Carousel.Item>{" "}
-            <Carousel.Item interval={1500}>
-              <img
-                className="d-block w-100"
-                src="https://p4.wallpaperbetter.com/wallpaper/529/666/472/advertising-logo-nike-poster-wallpaper-preview.jpg"
-                alt="Third slide"
-              />{" "}
-            </Carousel.Item>{" "}
-          </Carousel>
-        </Row>
-      </Col>
-    </Container>
+    <Row className="d-flex justify-content-center">
+      <Button
+        className="mb-3 mt-5  btn-categories"
+        variant="outline-secondary "
+        onClick={() => setOpen(!open)}
+        aria-controls="example-collapse-text"
+        aria-expanded={open}
+      >
+        Categorías
+      </Button>
+      <Collapse className="collapse-estilos" in={open}>
+        <div id="example-collapse-text  ">
+          <ListGroup className=" mb-5 list-categories">
+            <ListGroup.Item className="item-list-categories link-category ">
+              Fútbol
+            </ListGroup.Item>
+            <ListGroup.Item className="item-list-categories link-category ">
+              Tenis
+            </ListGroup.Item>
+            <ListGroup.Item className="item-list-categories link-category">
+              Rugby
+            </ListGroup.Item>
+            <ListGroup.Item className="item-list-categories link-category">
+              Atletismo
+            </ListGroup.Item>
+          </ListGroup>
+        </div>
+      </Collapse>
+    </Row>
   );
 };
 
