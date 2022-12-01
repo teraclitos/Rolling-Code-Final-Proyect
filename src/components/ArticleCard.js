ec935d767afce10a4758db123a192e83cc305e80
teraclitos <tefans12@gmail.com>
2022-12-01 13:41:49 -0300

    limito el numero de columnas a 2 por cuestiones estèticas

--- src/components/ArticleCard.js
+++ src/components/ArticleCard.js
@@ -8,43 +8,41 @@ import React from "react";
 
 const ArticleCard = ({ cart, d, add, auth }) => {
   return (
-    <div>
-      <Card className=" h-100 card-grid">
-        <Card.Img src={d.img_URL} variant="top" className=" img-card" />
-        <Card.Body className="p-0 ps-1 card-body  ">
-          <h3 className="category-title fs-6 mt-2 mb-0">{d.category}</h3>
-          <Card.Title className="mt-2 mb-0 card-title">{d.title}</Card.Title>
-          <Card.Text className="mt-1 mb-0 text-card-container ">
-            <p className="text-card">{d.description}</p>
-          </Card.Text>
-        </Card.Body>
-        <Card.Footer className="d-flex justify-content-between p-0 px-1 border-0 mt-2 bg-white card-footer ">
-          <Link
-            className="btn-reed mb-2"
-            to={
-              auth.user === "admin" || auth.user === "user"
-                ? `/ArticleDetailContainer/${d.id}`
-                : "/"
-            }
+    <Card className=" h-100 card-container">
+      <Card.Img src={d.img_URL} variant="top" className=" img-card" />
+      <Card.Body className="p-0 ps-1 card-body  ">
+        <h3 className="category-title fs-6 mt-2 mb-0">{d.category}</h3>
+        <Card.Title className="mt-2 mb-0 card-title">{d.title}</Card.Title>
+        <Card.Text className="mt-1 mb-0 text-card-container ">
+          <p className="text-card">{d.description}</p>
+        </Card.Text>
+      </Card.Body>
+      <Card.Footer className="d-flex justify-content-between p-0 px-1 border-0 mt-2 bg-white card-footer ">
+        <Link
+          className="btn-reed mb-2"
+          to={
+            auth.user === "admin" || auth.user === "user"
+              ? `/ArticleDetailContainer/${d.id}`
+              : "/"
+          }
+        >
+          {/* <Button className="py-1 px-2 btn-color ">Leer más</Button> */}
+          Leer más
+        </Link>
+        {auth.user === "user" && (
+          <Button
+            disabled={cart.includes(d)}
+            onClick={() => add(d)}
+            className="btn-like "
           >
-            {/* <Button className="py-1 px-2 btn-color ">Leer más</Button> */}
-            Leer más
-          </Link>
-          {auth.user === "user" && (
-            <Button
-              disabled={cart.includes(d)}
-              onClick={() => add(d)}
-              className="btn-like"
-            >
-              <FontAwesomeIcon
-                className="align-self-center fs-5 text-danger"
-                icon={faHeart}
-              />
-            </Button>
-          )}
-        </Card.Footer>
-      </Card>
-    </div>
+            <FontAwesomeIcon
+              className="align-self-center fs-5 text-danger"
+              icon={faHeart}
+            />
+          </Button>
+        )}
+      </Card.Footer>
+    </Card>
   );
 };
 
