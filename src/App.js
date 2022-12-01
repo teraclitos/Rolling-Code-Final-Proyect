7b17f7d059b3635a49894668f501c91315f48ef6
teraclitos <tefans12@gmail.com>
2022-12-01 11:59:19 -0300

    doy funcionalidad a las categorias

--- src/App.js
+++ src/App.js
@@ -148,7 +148,20 @@ function App() {
           />
           <Route path="/contacto" element={<Contact />} />
           <Route path="/usertable" element={<AdminTable />} />
-          <Route path="/category" element={<CategoryDetail data={data} />} />
+          <Route
+            path="/category"
+            element={
+              <CategoryDetail
+                data={data}
+                add={add}
+                cart={cart}
+                auth={auth}
+                validate={validate}
+                login={login}
+                logout={logout}
+              />
+            }
+          />
           <Route
             path="/ArticleDetailContainer/:id"
             element={
--- src/components/AsideAdvertisement.js
+++ src/components/AsideAdvertisement.js
@@ -1,11 +1,11 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
 import Carousel from "react-bootstrap/Carousel";
 
 const AsideAdvertisement = () => {
   return (
     <>
       <Carousel
-        className="carousel-advertisement sticky-top"
+        className="sticky-top carousel-advertisement-scroll-top"
         controls={false}
         indicators={false}
         slide={false}
@@ -15,14 +15,14 @@ const AsideAdvertisement = () => {
         <Carousel.Item interval={1500}>
           <img
             src="\publicidad-first.png"
-            className="d-block w-100"
+            className="d-block w-100  "
             alt="First slide"
           />
           <Carousel.Caption></Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item interval={1500}>
           <img
-            className="d-block w-100"
+            className="d-block w-100 "
             src="\publicidad-2.png"
             alt="Second slide"
           />
@@ -30,35 +30,35 @@ const AsideAdvertisement = () => {
         </Carousel.Item>
         <Carousel.Item interval={1500}>
           <img
-            className="d-block w-100"
+            className="d-block w-100 "
             src="\publicidad-third.png"
             alt="Third slide"
           />
         </Carousel.Item>
         <Carousel.Item interval={1500}>
           <img
-            className="d-block w-100"
+            className="d-block w-100 "
             src="\publicad-4-2-3.png"
             alt="fourth slide"
           />
         </Carousel.Item>
         <Carousel.Item interval={1500}>
           <img
-            className="d-block w-100"
+            className="d-block w-100 "
             src="\publicidad-4-medio.png"
             alt="fifth slide"
           />
         </Carousel.Item>
         <Carousel.Item interval={1500}>
           <img
-            className="d-block w-100"
+            className="d-block w-100 "
             src="\publicidad-4-3.png"
             alt="sixth slide"
           />
         </Carousel.Item>
         <Carousel.Item interval={1500}>
           <img
-            className="d-block w-100"
+            className="d-block w-100 "
             src="\publicidad-5.png"
             alt="seventh slide"
           />
