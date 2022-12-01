db536258a23d645263d07c1b4fa044286f474d8a
andreahongn <andreahongn@gmail.com>
2022-12-01 16:11:58 -0300

    agrego API a detail container

--- src/components/ArticleDetailContainer.js
+++ src/components/ArticleDetailContainer.js
@@ -2,26 +2,24 @@ import React, { useState, useEffect } from "react";
 import ArticleDetail from "./ArticleDetail";
 import { Route, Routes, Link, useParams } from "react-router-dom";
 import Highlights from "./Highlights";
-const ArticleDetailContainer = ({ add, cart }) => {
+const ArticleDetailContainer = ({ add, cart, auth }) => {
   const params = useParams();
   console.log("params" + params);
   const [data, setData] = useState([]);
-  useEffect(() => {
-    fetch(`https://fakestoreapi.com/products/${params.id}`)
-      .then((res) => res.json())
-      .then((json) => setData(json));
-  }, []);
   // useEffect(() => {
-  //   fetch(
-  //     `https://backend-news-3bqq4jzze-andreahongn.vercel.app/newslist/${params.id}`
-  //   )
+  //   fetch(`https://fakestoreapi.com/products/${params.id}`)
   //     .then((res) => res.json())
   //     .then((json) => setData(json));
-  // }, [params.id]);
+  // }, []);
+  useEffect(() => {
+    fetch(`https://backend-news-b7li2s1zo-andreahongn.vercel.app/${params.id}`)
+      .then((res) => res.json())
+      .then((json) => setData(json));
+  }, [params.id]);
 
   return (
     <div>
-      <ArticleDetail data={data} add={add} cart={cart} />;
+      <ArticleDetail data={data} add={add} cart={cart} auth={auth} />;
     </div>
   );
 };
