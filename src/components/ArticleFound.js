ec5039153b1e48621302974faf42a82c05304abb
teraclitos <tefans12@gmail.com>
2022-12-01 12:46:11 -0300

    importoto articleCard al searcher

--- src/components/ArticleFound.js
+++ src/components/ArticleFound.js
@@ -4,8 +4,9 @@ import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Button, Card, Pagination } from "react-bootstrap";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import ArticleCard from "./ArticleCard";
 
-const ArticleFound = ({ data }) => {
+const ArticleFound = ({ data, add, cart, auth }) => {
   const [stock, setStock] = useState([]);
   const [search, setSearch] = useState("");
 
@@ -35,7 +36,6 @@ const ArticleFound = ({ data }) => {
         <Form className="m-3">
           <InputGroup className="mb-3">
             <InputGroup.Text className="color-span">
-              {" "}
               <FontAwesomeIcon
                 style={{ fontSize: "2em", color: "#1986a0" }}
                 icon={faMagnifyingGlass}
@@ -47,41 +47,16 @@ const ArticleFound = ({ data }) => {
               className="me-2"
               aria-label="Search"
               onChange={handleChange}
-            />{" "}
+            />
           </InputGroup>
         </Form>
         <Container>
           {search && <h3 className="card-title">Resultados de Búsqueda</h3>}
           <Container>
             <Row lg={4}>
-              {" "}
               {stock.map((d, i) => (
                 <div className="mb-3  ">
-                  <Card className=" h-100 card-grid">
-                    <Card.Img
-                      src={d.img_URL}
-                      variant="top"
-                      className=" img-card"
-                    />
-                    <Card.Body className="p-0 ps-1 card-body  ">
-                      <h3 className="category-title fs-6 mt-2 mb-0">
-                        {d.category}
-                      </h3>
-                      <Card.Title className="mt-2 mb-0 card-title">
-                        {d.title}
-                      </Card.Title>
-                      <Card.Text className="mt-1 mb-0 text-card-container ">
-                        <p className="text-card">{d.description}</p>
-                      </Card.Text>
-                    </Card.Body>
-                    <Card.Footer className="d-flex justify-content-between p-0 px-1 border-0 mt-2 bg-white card-footer ">
-                      <Link to={`/ArticleDetailContainer/${d.id}`}>
-                        <Button className="py-1 px-2 btn-reed mb-2 ">
-                          Leer más
-                        </Button>
-                      </Link>
-                    </Card.Footer>
-                  </Card>
+                  <ArticleCard cart={cart} d={d} add={add} auth={auth} />
                 </div>
               ))}
             </Row>
