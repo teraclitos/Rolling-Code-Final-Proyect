ed20e6c8a585ac8de2f909115a1dd61e80b1d5d4
Marina Bianconi <marinabi2306@gmail.com>
2022-12-02 23:53:14 -0300

    coloco iconos

--- src/components/AdminTable.js
+++ src/components/AdminTable.js
@@ -1,44 +1,95 @@
 import { Container, Button } from "react-bootstrap";
-
+import React, { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "../styles/allcss.css";
 import { faTrash, faUserPen } from "@fortawesome/free-solid-svg-icons";
+import {
+  faUser,
+  faEnvelope,
+  faPenToSquare,
+} from "@fortawesome/free-solid-svg-icons";
 import Table from "react-bootstrap/Table";
+import Form from "react-bootstrap/Form";
+import Modal from "react-bootstrap/Modal";
 
 function AdminTable() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
     <body>
-      <Container>
-        <div class="d-flex mx-auto pt-4 pb-3 justify-content-center table-admin">
-          <Table striped bordered hover>
-            <thead>
-              <tr>
-                <th>#</th>
-                <th>First Name</th>
-                <th>Last Name</th>
-                <th>Username</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>1</td>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-              </tr>
-              <tr>
-                <td>2</td>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-              </tr>
-              <tr>
-                <td>3</td>
-                <td colSpan={2}>Larry the Bird</td>
-                <td>@twitter</td>
-              </tr>
-            </tbody>
-          </Table>
+      <Container fluid className="resposive-table">
+        <div class="d-flex flex-column">
+          <div class="d-flex flex-column align-items-center">
+            <h3 class="text-center py-2 titulo-admin">ADMINISTRAR USUARIO</h3>
+            <Table striped bordered hover className="color-table">
+              <thead>
+                <tr>
+                  <th>Id</th>
+                  <th>Usuario</th>
+                  <th>Modificar</th>
+                  <th>Eliminar</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1</td>
+                  <td>Mark</td>
+                  <td>
+                    <FontAwesomeIcon
+                      onClick={handleShow}
+                      style={{ fontSize: "1em", color: "#1986a0" }}
+                      icon={faUser}
+                    />
+
+                    <Modal show={show} onHide={handleClose}>
+                      <Modal.Header closeButton>
+                        <Modal.Title>Modal heading</Modal.Title>
+                      </Modal.Header>
+                      <Modal.Body>
+                        <Form>
+                          <Form.Group
+                            className="mb-3"
+                            controlId="exampleForm.ControlInput1"
+                          >
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control
+                              type="email"
+                              placeholder="name@example.com"
+                              autoFocus
+                            />
+                          </Form.Group>
+                          <Form.Group
+                            className="mb-3"
+                            controlId="exampleForm.ControlTextarea1"
+                          >
+                            <Form.Label>Example textarea</Form.Label>
+                            <Form.Control as="textarea" rows={3} />
+                          </Form.Group>
+                        </Form>
+                      </Modal.Body>
+                      <Modal.Footer>
+                        <Button variant="secondary" onClick={handleClose}>
+                          Close
+                        </Button>
+                        <Button variant="primary" onClick={handleClose}>
+                          Save Changes
+                        </Button>
+                      </Modal.Footer>
+                    </Modal>
+                  </td>
+                  <td>@mdo</td>
+                </tr>
+                <tr>
+                  <td>2</td>
+                  <td>Jacob</td>
+                  <td>Thornton</td>
+                  <td>@fat</td>
+                </tr>
+              </tbody>
+            </Table>
+          </div>
         </div>
       </Container>
     </body>
