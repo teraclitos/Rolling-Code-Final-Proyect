2ffc567d62732dcdf274244dcc65594009d7e6c9
Marina Bianconi <marinabi2306@gmail.com>
2022-12-05 00:34:16 -0300

    corrijo resposive y footer

--- src/views/Main.js
+++ src/views/Main.js
@@ -12,7 +12,7 @@ import RecoverPass from "../components/RecoverPass";
 import Error404 from "../components/Error404";
 import Loader from "../components/Loader";
 import { Route, Routes } from "react-router-dom";
-
+import { Container, Row, Form, InputGroup } from "react-bootstrap";
 const Main = ({
   toastError,
   toastSuccess,
@@ -33,118 +33,120 @@ const Main = ({
 }) => {
   return (
     <>
-      <Navbar
-        toastError={toastError}
-        toastSuccess={toastSuccess}
-        cart={cart}
-        del={del}
-        clear={clear}
-        data={data}
-        dataUser={dataUser}
-        auth={auth}
-        validate={validate}
-        login={login}
-        logout={logout}
-        handleShowLogin={handleShowLogin}
-        showLogin={showLogin}
-        setShowLogin={setShowLogin}
-      />
-      <Routes>
-        <Route
-          path="/highlights"
-          element={
-            <Highlights
-              toastError={toastError}
-              toastSuccess={toastSuccess}
-              data={data}
-              add={add}
-              cart={cart}
-              totalHighlights={totalHighlights}
-            />
-          }
+      <Container fluid className="d-flex flex-column min-vh-100 px-0">
+        <Navbar
+          toastError={toastError}
+          toastSuccess={toastSuccess}
+          cart={cart}
+          del={del}
+          clear={clear}
+          data={data}
+          dataUser={dataUser}
+          auth={auth}
+          validate={validate}
+          login={login}
+          logout={logout}
+          handleShowLogin={handleShowLogin}
+          showLogin={showLogin}
+          setShowLogin={setShowLogin}
         />
-        <Route
-          path="/articlefound"
-          element={
-            <ArticleFound
-              data={data}
-              add={add}
-              cart={cart}
-              auth={auth}
-              validate={validate}
-              login={login}
-              logout={logout}
-            />
-          }
-        />
-        <Route
-          path="/"
-          element={
-            <Articlepublicitygrid
-              data={data}
-              add={add}
-              cart={cart}
-              auth={auth}
-              validate={validate}
-              login={login}
-              logout={logout}
-              toastError={toastError}
-              totalHighlights={totalHighlights}
-              handleShowLogin={handleShowLogin}
-            />
-          }
-        />
-        <Route
-          path="/contacto"
-          element={<Contact toastSuccess={toastSuccess} />}
-        />
-        <Route
-          path="/usertable"
-          element={
-            <AdminTable
-              dataUser={dataUser}
-              toastError={toastError}
-              toastSuccess={toastSuccess}
-            />
-          }
-        />
+        <Routes>
+          <Route
+            path="/highlights"
+            element={
+              <Highlights
+                toastError={toastError}
+                toastSuccess={toastSuccess}
+                data={data}
+                add={add}
+                cart={cart}
+                totalHighlights={totalHighlights}
+              />
+            }
+          />
+          <Route
+            path="/articlefound"
+            element={
+              <ArticleFound
+                data={data}
+                add={add}
+                cart={cart}
+                auth={auth}
+                validate={validate}
+                login={login}
+                logout={logout}
+              />
+            }
+          />
+          <Route
+            path="/"
+            element={
+              <Articlepublicitygrid
+                data={data}
+                add={add}
+                cart={cart}
+                auth={auth}
+                validate={validate}
+                login={login}
+                logout={logout}
+                toastError={toastError}
+                totalHighlights={totalHighlights}
+                handleShowLogin={handleShowLogin}
+              />
+            }
+          />
+          <Route
+            path="/contacto"
+            element={<Contact toastSuccess={toastSuccess} />}
+          />
+          <Route
+            path="/usertable"
+            element={
+              <AdminTable
+                dataUser={dataUser}
+                toastError={toastError}
+                toastSuccess={toastSuccess}
+              />
+            }
+          />
 
-        <Route
-          path="/category"
-          element={
-            <CategoryDetail
-              data={data}
-              add={add}
-              cart={cart}
-              auth={auth}
-              validate={validate}
-              login={login}
-              logout={logout}
-            />
-          }
-        />
+          <Route
+            path="/category"
+            element={
+              <CategoryDetail
+                data={data}
+                add={add}
+                cart={cart}
+                auth={auth}
+                validate={validate}
+                login={login}
+                logout={logout}
+              />
+            }
+          />
 
-        <Route
-          path="/ArticleDetailContainer/:id"
-          element={
-            <ArticleDetailContainer
-              add={add}
-              cart={cart}
-              auth={auth}
-              validate={validate}
-              login={login}
-              logout={logout}
-              toastError={toastError}
-              toastSuccess={toastSuccess}
-              totalHighlights={totalHighlights}
-              dataTotal={data}
-            />
-          }
-        />
-        <Route path="/recuperarContraseña" element={<RecoverPass />} />
-        <Route path="/error404" element={<Error404 />} />
-      </Routes>
-      <Footer />
+          <Route
+            path="/ArticleDetailContainer/:id"
+            element={
+              <ArticleDetailContainer
+                add={add}
+                cart={cart}
+                auth={auth}
+                validate={validate}
+                login={login}
+                logout={logout}
+                toastError={toastError}
+                toastSuccess={toastSuccess}
+                totalHighlights={totalHighlights}
+                dataTotal={data}
+              />
+            }
+          />
+          <Route path="/recuperarContraseña" element={<RecoverPass />} />
+          <Route path="/error404" element={<Error404 />} />
+        </Routes>
+        <Footer />
+      </Container>
     </>
   );
 };
